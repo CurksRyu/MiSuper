3738bba5239da9f00d3dd23bdfaa1e054825ef75
Jorge <jorgexm99@gmail.com>
2021-12-03 22:11:10 -0300

    Interacciones iniciales de pagina añadidas

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,28 @@
-$(window).scroll(function() {    
-    posicionarMenu();
+//libreria Jquery
+$(document).ready(() => {
+    $("#displayCategories").hide();
+    $("#displaySup").hide();
+
+    $("#mostrarCat").on("click", function() {
+        $("#displayCategories").toggle(200);
+    });
+
+    $("#mostrarSup").on("click", function() {
+        $("#displaySup").toggle(200);
+    });
+
+    $("#displaySup").mouseleave(function(){
+        $("#displaySup").hide(200);
+    });
+
+    $("#displayCategories").mouseleave(function(){
+        $("#displayCategories").hide(200);
+    });
+
+    $(".navbar-burger").click(function() {
+
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        $(".navbar-burger").toggleClass("is-active");
+        $(".navbar-menu").toggleClass("is-active");
+    });
 });
-
-function posicionarMenu() {
-    var altura_del_header = $('.header').outerHeight(true);
-    var altura_del_menu = $('.menu').outerHeight(true);
-
-    if ($(window).scrollTop() >= altura_del_header){
-        $('.menu').addClass('fixed');
-        $('.wrapper').css('margin-top', (altura_del_menu) + 'px');
-    } else {
-        $('.menu').removeClass('fixed');
-        $('.wrapper').css('margin-top', '0');
-    }
-}
